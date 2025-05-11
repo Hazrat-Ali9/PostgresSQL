a463e345e266e7a2ea41a3986bd55567226d0f7b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-11 12:01:09 +0600

    index   🌺

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import app from "./app";
-
+// index cheak the server
 const prisma = new PrismaClient();
 const port = process.env.PORT || 5000;
 async function main() {
